dea01a11e2e11a316904e8ba4cb93de7b53e1a52
anirbandotdev <anirbaniitian@gmail.com>
2026-02-24 23:07:32 +0530

    Separate io functions into io.asm file

--- utils.asm
+++ utils.asm
@@ -13,45 +13,6 @@ done:
 	pop ebx
 	ret
 
-print:
-	; print without a newline
-	push edx
-	push ecx
-	push ebx
-	push eax
-	call strlen
-
-	mov edx , eax
-
-	pop eax
-
-	mov ecx , eax
-	mov ebx , 1
-	mov eax , 4
-	int 80h
-
-	pop ebx
-	pop ecx
-	pop edx
-	ret 
-
-sprint:
-	; print with a newline
-	call print
-	push eax
-
-	mov eax , 0Ah
-	push eax
-
-	mov eax , esp
-	call print
-	
-	pop eax
-	pop eax
-	ret
-
-
-
 quit:
 	mov eax , 1
 	mov ebx , 0
