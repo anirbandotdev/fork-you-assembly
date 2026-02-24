4185207680b9d70b33021ea6c69c6a20ab3db041
anirbandotdev <anirbaniitian@gmail.com>
2026-02-24 22:46:06 +0530

    Add subroutine for calculating length of the string

--- utils.asm
+++ utils.asm
@@ -1,3 +1,24 @@
+strlen:
+	push ebx 
+	mov ebx , eax
+
+nextchar:
+	cmp byte [eax] , 0
+	jz done
+	inc eax 
+	jmp nextchar
+
+done:
+	sub eax , ebx
+	pop ebx
+	ret
+
+print:
+
+
+sprint:
+
+
 quit:
 	mov eax , 1
 	mov ebx , 0
